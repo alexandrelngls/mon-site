b4c49d0be30c7785b4554550782d1b40db8e4493
alexandrelngls <alexandre.lngls@gmail.com>
2025-06-28 22:00:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-const supabaseUrl = 'https://nqrtrpsommuudwoakgcp.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xcnRycHNvbW11dWR3b2FrZ2NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExMzMwODEsImV4cCI6MjA2NjcwOTA4MX0.lS2nRqBAlUdV-B9-5Amjn31p_cO7M0jzj9YZ2_v8b3M';
-const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
+const supabaseClient = window.supabaseClient;
 
 async function seConnecter() {
   const email = document.getElementById('email').value.trim();
