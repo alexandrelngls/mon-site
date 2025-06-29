e839e8b61a13628be9d6803e6a14daa01f6b3cf8
alexandrelngls <alexandre.lngls@gmail.com>
2025-06-29 12:39:24 +0200

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,11 @@
+export async function login(email, password) {
+  const resp = await fetch('https://secure.alexandrelanglois.me/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password })
+  });
+
+  return resp;
+}
