192c5cd789109884973e8105ac0896665777a9f2
alexandrelngls <alexandre.lngls@gmail.com>
2025-06-29 15:12:22 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,9 +1,7 @@
 export async function login(email, password) {
   return fetch('https://secure.alexandrelanglois.me/', {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ email, password })
   });
 }
