f367804e3188cf1debe2ff53f0c9c502cdec9f0d
alexandrelngls <alexandre.lngls@gmail.com>
2025-06-29 14:50:25 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,11 +1,9 @@
 export async function login(email, password) {
-  const resp = await fetch('https://secure.alexandrelanglois.me/', {
+  return fetch('https://secure.alexandrelanglois.me/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ email, password })
   });
-
-  return resp;
 }
