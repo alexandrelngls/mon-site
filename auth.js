9f96c3d73d8bfde07d732521477c449601166be0
alexandrelngls <alexandre.lngls@gmail.com>
2025-07-03 14:58:48 +0200

    Delete auth.js

--- auth.js
+++
@@ -1,7 +0,0 @@
-export async function login(email, password) {
-  return fetch('https://secure.alexandrelanglois.me/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email, password })
-  });
-}
